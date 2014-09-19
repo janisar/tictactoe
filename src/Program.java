e0a4391eb3246d2bdd36b51e21507001494448cf
Janis Ardel <ardeljanis@gmail.com>
2014-09-19 20:16:40 +0300

    refactored some code

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Program {
-	static int[] m = new int[9];
-	static int[] cpuPreferedMoves = new int[] { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
+	static int[] table = new int[9];
 	static int[][] possibleWins = new int[][] { { 0, 2, 1 }, { 4, 3, 5 },
-		{ 6, 8, 7 }, // ROWS
-		{ 0, 6, 3 }, { 4, 1, 7 }, { 2, 8, 5 }, // COLUMNS
-		{ 4, 0, 8 }, { 4, 2, 6 } }; // DIAGONALS
+			{ 6, 8, 7 }, // ROWS
+			{ 0, 6, 3 }, { 4, 1, 7 }, { 2, 8, 5 }, // COLUMNS
+			{ 4, 0, 8 }, { 4, 2, 6 } }; // DIAGONALS
 	private static final int SQUARES_IN_ROW = 3;
 	private static final int PLAYER = 1;
 	private static final int EMPTY = 0;
 	private static final int CPU = -1;
+	private static final int WIN = 3;
 
 	public static void main(String args[]) {
 		showBoard();
@@ -22,25 +22,25 @@ public class Program {
 		while (boardHasEmptySquare()) {
 			userTurn(scanner);
 			showCurrentBoard();
-			checkWin(m);
+			checkWin(table);
 			if (boardHasEmptySquare()) {
 				cpuTurn();
 				showCurrentBoard();
-				checkWin(m);
+				checkWin(table);
 			}
 		}
 		scanner.close();
 	}
 
 	private static void showCurrentBoard() {
-		for (int i = 0; i < m.length; i += SQUARES_IN_ROW) {
+		for (int i = 0; i < table.length; i += SQUARES_IN_ROW) {
 			System.out.print("+---+---+---+\n");
 			System.out.print("| ");
-			printSquare(m[i]);
+			printSquare(table[i]);
 			System.out.print(" | ");
-			printSquare(m[i + 1]);
+			printSquare(table[i + 1]);
 			System.out.print(" | ");
-			printSquare(m[i + 2]);
+			printSquare(table[i + 2]);
 			System.out.println(" | ");
 		}
 		System.out.println("+---+---+---+");
@@ -58,18 +58,18 @@ public class Program {
 
 	private static void cpuTurn() {
 		System.out.println("Arvuti kord: ");
-		int cpuMove = makeMove(m);
-		m[cpuMove] = CPU;
+		int cpuMove = makeMove(table);
+		table[cpuMove] = CPU;
 	}
 
 	private static void userTurn(Scanner scanner) {
 		System.out.println("Make your move.");
 		int userMove = readInput(scanner);
-		m[userMove] = PLAYER;
+		table[userMove] = PLAYER;
 	}
 
 	private static boolean boardHasEmptySquare() {
-		for (int i : m) {
+		for (int i : table) {
 			if (i == EMPTY) {
 				return true;
 			}
@@ -87,15 +87,15 @@ public class Program {
 				correctMove = true;
 				if (userMove > 9 || userMove < 0) {
 					System.out
-					.println("Out of bounds. Choose a number from 0-8");
+							.println("Out of bounds. Choose a number from 0-8");
 					correctMove = false;
-				} else if (m[userMove] != 0) {
+				} else if (table[userMove] != 0) {
 					System.out.println("That square has been filled.");
 					correctMove = false;
 				}
 			} catch (NumberFormatException ex) {
 				System.out
-				.println("That was not a number. Please choose a number from 0-8.");
+						.println("That was not a number. Please choose a number from 0-8.");
 			}
 		}
 		return userMove;
@@ -108,9 +108,9 @@ public class Program {
 			int currentResult = 0;
 			boolean canMove = false;
 			for (int j = 0; j < possibleWins[i].length; j++) {
-				if (m[possibleWins[i][j]] == PLAYER) {
+				if (table[possibleWins[i][j]] == PLAYER) {
 					currentResult -= 3;
-				} else if (m[possibleWins[i][j]] == CPU) {
+				} else if (table[possibleWins[i][j]] == CPU) {
 					currentResult += 4;
 				} else {
 					canMove = true;
@@ -126,7 +126,7 @@ public class Program {
 
 	private static int calculateRow(int[] row) {
 		for (int i : row) {
-			if (m[i] == EMPTY) {
+			if (table[i] == EMPTY) {
 				return i;
 			}
 		}
@@ -137,16 +137,16 @@ public class Program {
 		int win = 0;
 		for (int i = 0; i < possibleWins.length; i++) {
 			for (int j = 0; j < possibleWins[i].length; j++) {
-				if (m[possibleWins[i][j]] == CPU) {
+				if (table[possibleWins[i][j]] == CPU) {
 					win++;
-				} else if (m[possibleWins[i][j]] == PLAYER) {
+				} else if (table[possibleWins[i][j]] == PLAYER) {
 					win--;
 				}
 			}
-			if (win == -3) {
+			if (win == -WIN) {
 				System.out.println("Sinu võit");
 				System.exit(1);
-			} else if (win == 3) {
+			} else if (win == WIN) {
 				System.out.println("Arvuti võit");
 				System.exit(1);
 			} else {
@@ -157,31 +157,13 @@ public class Program {
 
 	public static void showBoard() {
 		System.out.println("This is the layout of the board. You play as X.");
-		System.out.print("+---+---+---+\n");
-		System.out.print("| ");
-		System.out.print("0");
-		System.out.print(" | ");
-		System.out.print("1");
-		System.out.print(" | ");
-		System.out.print("2");
-		System.out.println(" | ");
 		System.out.println("+---+---+---+");
-		System.out.print("| ");
-		System.out.print("3");
-		System.out.print(" | ");
-		System.out.print("4");
-		System.out.print(" | ");
-		System.out.print("5");
-		System.out.println(" | ");
-		System.out.print("+---+---+---+\n");
-		System.out.print("| ");
-		System.out.print("6");
-		System.out.print(" | ");
-		System.out.print("7");
-		System.out.print(" | ");
-		System.out.print("8");
-		System.out.println(" | ");
-		System.out.print("+---+---+---+\n");
+		System.out.println("| 0 | 1 | 2 |");
+		System.out.println("+---+---+---+");
+		System.out.println("| 3 | 4 | 5 |");
+		System.out.println("+---+---+---+");
+		System.out.println("| 6 | 7 | 8 |");
+		System.out.println("+---+---+---+");
 		System.out.println("Get ready for the game!");
 	}
 }
